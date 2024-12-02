295aaf521f0c507a199f99f34c58909a8be9fc76
Jay-ganesh <132564206+Jay-ganesh@users.noreply.github.com>
2024-12-02 16:18:42 +0530

    Password

--- src/User/UserRegistration.java
+++ src/User/UserRegistration.java
@@ -9,6 +9,7 @@ public class UserRegistration {
         String nameRegex = "^[A-Z][a-z]{2,}$";
         String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
         String mobileRegex = "^[0-9]{2} [0-9]{10}$";
+        String passwordRegex = "^.{8,}&";
 
         System.out.println("Enter the first name : ");
         String firstName = sc.nextLine();
@@ -25,6 +26,10 @@ public class UserRegistration {
         System.out.println("Enter the phone number : ");
         String phoneNumber = sc.nextLine();
         validator(phoneNumber,mobileRegex,"PhoneNumber");
+
+        System.out.println("Enter the password : ");
+        String password = sc.nextLine();
+        validator(password,passwordRegex,"Password");
     }
     public static void validator(String name, String pattern, String nameType){
         if (Pattern.matches(pattern,name)){
